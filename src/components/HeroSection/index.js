863af79bd51e6a581e62398db5ac4c132de500b8
KawtherSaleh99 <kawtherwsaleh@gmail.com>
2022-05-23 20:10:58 +0300

    First commit

---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import Video from "../../videos/video.mp4";
+import { Button } from "../ButtonElement";
+import {
+  HeroBg,
+  HeroContainer,
+  VideoBg,
+  HeroContent,
+  HeroH1,
+  HeroP,
+  HeroBtnWrapper,
+  ArrowForward,
+  ArrowRight,
+} from "./HeroElements";
+const HeroSection = () => {
+  const [hover, setHover] = useState(false);
+
+  const onHover = () => {
+    setHover(!hover);
+  };
+  return (
+    <HeroContainer id="home">
+      <HeroBg>
+        <VideoBg autoPlay loop muted src={Video} type="video/mp4"></VideoBg>
+      </HeroBg>
+      <HeroContent>
+        <HeroH1>We guide you through</HeroH1>
+        <HeroP>Find your postgraduates courses in IIUM</HeroP>
+        <HeroBtnWrapper>
+          <Button to="signup" onMouseEnter={onHover} onMouseLeave={onHover}
+          primary= 'true'
+          dark= 'true'>
+            Get started {hover ? <ArrowForward /> : <ArrowRight />}
+          </Button>
+        </HeroBtnWrapper>
+      </HeroContent>
+    </HeroContainer>
+  );
+};
+
+export default HeroSection;
---
+++ src/components/NavBar/index.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { FaBars } from "react-icons/fa";
+import {
+  Nav,
+  NavBarContainer,
+  NavLogo,
+  MobileIcon,
+  NavMenu,
+  NavItem,
+  NavLinks,
+  NavBtn,
+  NavBtnLink,
+} from "./NavBarElements";
+
+const NavBar = ({ toggle }) => {
+  return (
+    <>
+      <Nav>
+        <NavBarContainer>
+          <NavLogo to="/">pguide</NavLogo>
+          <MobileIcon onClick={toggle}>
+            <FaBars />
+          </MobileIcon>
+          <NavMenu>
+            <NavItem>
+              <NavLinks to="about">About</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="programme">Programmes</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="staff">Staff</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="apply">Apply now</NavLinks>
+            </NavItem>
+          </NavMenu>
+          <NavBtn>
+            <NavBtnLink to="/signin">Sign In</NavBtnLink>
+          </NavBtn>
+        </NavBarContainer>
+      </Nav>
+    </>
+  );
+};
+
+export default NavBar;
