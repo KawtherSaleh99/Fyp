2cdf26aad40e63fdf2cdcec28cab0e6cc353293d
KawtherSaleh99 <106105834+KawtherSaleh99@users.noreply.github.com>
2022-06-17 03:28:09 +0800

    Revert "change button element to div"
    This reverts commit ec70a203e42d675de1ae4cb16670b3aca17c5d3f.

--- src/components/ButtonElement.js
+++ src/components/ButtonElement.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
-import { Link } from "react-router-dom";
+import { Link } from "react-scroll";
 
-export const Button = styled.div`
+export const Button = styled(Link)`
   border-radius: 50px;
   background: ${({ primary }) => (primary ? "#00DCDC" : "#010606")};
   white-space: nowrap;
