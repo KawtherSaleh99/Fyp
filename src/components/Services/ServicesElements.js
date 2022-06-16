db0345ee498709af6fe56723f4243b6a97d25762
KawtherSaleh99 <106105834+KawtherSaleh99@users.noreply.github.com>
2022-06-17 03:28:09 +0800

    Revert "indention fix"
    This reverts commit 1b0cb4638cc8194c5f62dec9af6aff201891ab00.

--- src/components/Services/ServicesElements.js
+++ src/components/Services/ServicesElements.js
@@ -36,22 +36,22 @@ export const ServicesWrapper = styled.div`
 `;
 
 export const ServicesCard = styled.div`
-  background: #fff;
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  align-items: center;
-  border-radius: 10px;
-  max-height: 340px;
-  padding: 30px;
-  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.2);
-  transition: all 0.2s ease-in-out;
+    background: #fff;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+    border-radius: 10px;
+    max-height: 340px;
+    padding: 30px;
+    box-shadow: 0 1px 3px rgba(0,0,0,0.2);
+    transition: all 0.2s ease-in-out;
 
-  &:hover {
+&:hover {
     transform: scale(1.02);
     transition: all 0.2 ease-in-out;
     cursor: pointer;
-  }
+}
 `;
 
 export const ServicesIcon = styled.img`
