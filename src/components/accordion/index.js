3adab5301a9dde933b0edc356434ae9f92618266
KawtherSaleh99 <kawtherwsaleh@gmail.com>
2022-06-17 16:13:37 +0800

    TEXTS

--- src/components/accordion/index.js
+++ src/components/accordion/index.js
@@ -5,6 +5,8 @@ import {
   AccordionPanel,
   AccordionIcon,
   Box,
+  Container,
+  Text
 } from "@chakra-ui/react";
 
 export const AccordionComponent = ({ title, content }) => {
@@ -12,24 +14,28 @@ export const AccordionComponent = ({ title, content }) => {
   return (
     <>
       <h1>{content?.title}</h1>
-      <Accordion>
-        {Object.keys(content).map((key) => {
-          console.log("key -----> ", key);
-          return (
-            <AccordionItem>
-              <h2>
-                <AccordionButton>
-                  <Box flex="1" textAlign="left">
-                    {key}
-                  </Box>
-                  <AccordionIcon />
-                </AccordionButton>
-              </h2>
-              <AccordionPanel pb={4}>{content[key]}</AccordionPanel>
-            </AccordionItem>
-          );
-        })}
-      </Accordion>
+      <Container maxW="660px">
+        <Text fontSize={18}>
+        <Accordion>
+          {Object.keys(content).map((key) => {
+            console.log("key -----> ", key);
+            return (
+              <AccordionItem>
+                <h2>
+                  <AccordionButton>
+                    <Box flex="1" textAlign="left">
+                      {key}
+                    </Box>
+                    <AccordionIcon />
+                  </AccordionButton>
+                </h2>
+                <AccordionPanel pb={4}>{content[key]}</AccordionPanel>
+              </AccordionItem>
+            );
+          })}
+        </Accordion>
+        </Text>
+      </Container>
     </>
   );
 };
--- src/pages/programmes/departments.js
+++ src/pages/programmes/departments.js
@@ -3,7 +3,7 @@ const departments = [
     title: "Department of Information Systems",
     accordionTitle: "",
     description:
-      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidemodi reprehenderit vitae exercitationem aliquid dolores ullam temporibus enim expedita aperiam mollitia iure consectetur dictatenetur, porro consequuntur saepe accusantium consequatur.",
+      "The Department of Information Systems (DIS) was first established in 2001 under the Kulliyyah (Faculty) of Economics and Management (KENMS). The first programme offered was the Bachelor of Management Information Systems (BMIS). In July 2004, a new programme, Bachelor of Information Technology (BIT) was introduced to replace the BMIS programme. In October 2004, the BIT programme had received recognition by the Public Service Department of Malaysia. ",
     image: "https://www.ecpi.edu/sites/default/files/MS%20Tech%20Sept%2025.png",
     programmes: [
       {
@@ -29,7 +29,7 @@ const departments = [
   {
     title: "Department of Computer Science",
     description:
-      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidemodi reprehenderit vitae exercitationem aliquid dolores ullam temporibus enim expedita aperiam mollitia iure consectetur dictatenetur, porro consequuntur saepe accusantium consequatur.",
+      "The Department of Computer Science (DCS) was established in 2004, in the Kulliyyah of Information and Communication Technology (KICT). The first programme offered was the Bachelor of Computer Science (Honours) programme. This was followed by the postgraduate programmes in 2011, namely Master of Computer Science (MCS) and Ph.D. in Computer Science (Ph.D. CS).",
     image:
       "https://www.city.ac.uk/__data/assets/image/0005/587768/varieties/breakpoint-max.jpg",
     programmes: [
@@ -46,7 +46,7 @@ const departments = [
   {
     title: "Department of Library & Information Science",
     description:
-      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidemodi reprehenderit vitae exercitationem aliquid dolores ullam temporibus enim expedita aperiam mollitia iure consectetur dictatenetur, porro consequuntur saepe accusantium consequatur.",
+      "The Department of Library & Information Science was established in 1992 offering the Master in Library and Information Science (MLIS) degree to both full time and part time students. In the initial period, the programme was meant to upgrade the competency of existing librarians, while at the same time producing a new breed of competent informational professionals imbued with Islamic values to meet the diverse needs and demands of the job market. The doctoral programme (PhD) in Library and Information Science was added later.",
     image: "https://lis.catholic.edu/_media/2018-photos-LIS/OrgofInfo1200.jpg",
 
     programmes: [
